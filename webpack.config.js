4e20fd2dbbc7f5de01febcc04b1adb7a876f2056
walle7-com <46820012+walle7-com@users.noreply.github.com>
2019-05-04 12:47:18 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+	'bundle.css': [
+	    path.resolve(__dirname, 'src/assets/css/fonts.css'),
+	    path.resolve(__dirname, 'src/assets/css/load.css'),
+	    path.resolve(__dirname, 'src/assets/css/core.css')
+	],
+	'bundle.js': [
+	    '@babel/polyfill',
+	    path.resolve(__dirname, 'src/init.js')
+	],
+    },
+
+    output: {
+	filename: '[name]',
+	path: path.resolve(__dirname, 'dist'),
+    },
+
+    mode: 'development',
+    devtool: 'inline-source-map',
+
+    module: {
+	rules: [{
+	    test: /\.m?js$/,
+	    exclude: /(node_modules)/,
+	    use: {
+		loader: 'babel-loader',
+		options: {
+		    presets: ['@babel/preset-env'],
+		}
+	    }
+	}, {
+	    test: /\.css$/,
+	    use: ExtractTextPlugin.extract({
+		fallback: 'style-loader',
+		use: [{
+		    loader: 'css-loader',
+		    options: {
+			url: false
+		    }
+		}]
+	    })
+	}]
+    },
+    plugins: [
+	new ExtractTextPlugin('bundle.css'),
+	new HtmlWebpackPlugin({
+	    filename: 'index.html',
+	    template: 'src/assets/html/index.html',
+	    inject: false
+	}),
+	new CopyPlugin([{
+	    from: 'src/assets/i',
+	    to: 'i'
+	}]),
+    ]
+}
